2675c05881e2a8fe870f84c410f8575a198d561f
Tom Luo <mariolzx@gmail.com>
2021-04-09 12:35:55 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Tom Lou on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
