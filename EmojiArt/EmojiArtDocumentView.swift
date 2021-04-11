2ba296bdef35c2713e456fcd1f7277c8d83e81ae
Tom Luo <mariolzx@gmail.com>
2021-04-10 21:48:04 -0400

    part 2 lecture 7

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -16,24 +16,43 @@ struct EmojiArtDocumentView: View {
             ForEach(EmojiArtDocument.palette.map {String($0)}, id: \.self) {emoji in
                Text(emoji)
                 .font(Font.system(size: self.defaultEmojiSize))
+                .onDrag {return NSItemProvider(object: emoji as NSString)}
             }
             }
         }
         .padding(.horizontal)
-            Rectangle().foregroundColor(.yellow)
-                .edgesIgnoringSafeArea([.horizontal, .bottom])
-                .onDrop(of: ["public.image"], isTargeted: nil) { providers, location in
-                    return self.drop(providers: providers)
-                }
+            GeometryReader { geometry in
+                Color.white.overlay(
+                    Group {
+                        if self.document.backgroundImage != nil {
+                            Image(uiImage: self.document.backgroundImage!)
+                        }
+                    }
+                    )
+                    .edgesIgnoringSafeArea([.horizontal, .bottom])
+                    .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location in
+                        var location = geometry.convert(location, from: .global)
+                        location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                        return self.drop(providers: providers, at: location)
+                    }
+                
+            }
+
         }
         
     }
-    private func drop(providers: [NSItemProvider]) -> Bool {
-        let found = providers.loadFirstObject(ofType: URL.self) { url in
-            print("dropped \(url)")
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            //print("dropped \(url)")
             self.document.setBackgroundURL(url)
             
         }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) {string in
+                self.document.addEmoji(string, at: location, size: self.defaultEmojiSize)
+            }
+        }
+        
         return found
     }
     
